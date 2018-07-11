fde049f19c2615373b6ff6994f1f3fd470ae8773
Administrator <admin@example.com>
2018-07-11 15:38:19 +0300

    Seed change

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -95,7 +95,7 @@ public:
 		base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
 		base58Prefixes[EXT_SECRET_KEY] = list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
 
-		vSeeds.push_back(CDNSSeedData("0", "195.168.0.19"));
+		vSeeds.push_back(CDNSSeedData("0", "192.168.0.19"));
 	
 		convertSeeds(vFixedSeeds, pnSeed, ARRAYLEN(pnSeed), nDefaultPort);
 
